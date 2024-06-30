dc2db778fe5893b2a24c77304ffc839ba8bcb6d1
Omkar <omkarbathe555@gmail.com>
2024-06-30 21:47:34 +0530

    added mongo connection

--- rest.js
+++ rest.js
@@ -1,10 +1,50 @@
-const users = require("./MOCK_DATA.json");
+// const users = require("./MOCK_DATA.json");
 const express = require("express");
 const fs= require("fs");
 const { stringify } = require("querystring");
+const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose.connect("mongodb://127.0.0.1:27017/demo").then(()=>{
+  console.log("Mongoose connected");
+
+}).catch(err => console.log(err));
+
+const userSchema = new mongoose.Schema({
+  id:{
+    
+
+  },
+  firstname:
+   {
+    type: "string",
+    required : true,
+
+   },
+   lastname:
+   {
+    type:"string",
+    required : false,
+
+   },
+
+   email:{
+    type:"string",
+    required : true,
+    unique : true,
+
+
+   },
+   gender:{
+    type: "string",
+
+   }
+
+
+})
+
+const User = mongoose.model("user", userSchema);
 app.use(express.urlencoded({extended:false}));
 
 
@@ -13,30 +53,44 @@ app.use((req,res,next)=>{
    console.log("hello");
    next();//giving task to next middleware or function
 })
-app.get("/",(req,res)=>{
-  res.end("home page");
+app.get("/",async (req,res)=>{
+  const result = await User.find({});
+  res.status(200).json(result);
 })
-app.get("/user", (req, res) => {
-  return res.status(200).json(users);
+app.get("/user", async(req, res) => {
+  const result = await User.find({});
+  res.status(200).json(result);
 });
 
 app
-  .route("/user/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.status(200).json(user);
+  .route ("/user/:id")
+  .get(async (req, res) => {
+    const id = req.params.id;
+    const result = await User.findById(req.params.id)
+    // const user = users.find((user) => user.id === id);
+    return res.status(200).json(result);
   })
   .patch((req ,res)=>{
 
   });
-  app.post("/user",(req,res)=>{
-    user = req.body;
-    users.push(user);
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users),(err,data)=>{
-        return res.status(201).json({status:"success", id : users.length});
+  app.post("/user", async(req,res)=>{
+    body = req.body;
+    const result = await User.create(
+      {
+        id:body.id,
+        firstname:body.firstname,
+        lastname:body.lastname,
+        email : body.email,
+        gender: body.gender
+      }
+    )
+    console.log(result)
+    // users.push(user);
+    // fs.writeFile("./MOCK_DATA.json", JSON.stringify(users),(err,data)=>{
+        // return res.status(201).json({status:"success", id : users.length});
 
-    });
+    // });
+    return res.status(201).json(result);
     
   });
 app.listen(8000);
